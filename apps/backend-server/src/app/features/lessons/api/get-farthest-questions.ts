928feeda457cdd741ab5e1c84378437203c95c06
pawelfurman <pawelfurman@pawelfurmans-MacBook-Pro.local>
2023-03-19 00:42:10 +0100

    [] improvememtnts

--- apps/backend-server/src/app/features/lessons/api/get-farthest-questions.ts
+++ apps/backend-server/src/app/features/lessons/api/get-farthest-questions.ts
@@ -1,26 +1,67 @@
 import { Request, Response } from "express";
-import { Op, Sequelize } from "sequelize";
+import { col, fn, Op, Sequelize } from "sequelize";
+import { sequelize } from "../../../db/connection";
 import ILessons from "../../../db/models/lessons";
 import ILessonsStatus from "../../../db/models/lessonsStatus";
 import ILessonsStep from "../../../db/models/lessonsStep";
+import IQuestion from "../../../db/models/questions";
 
 export const getFarthesQuestions = async (req:Request, res: Response) => {
-    const amount = req.query.amount;
+    const amount = Number(req.query.amount);
     const userId = res.locals.userId;
 
     const fromSteps = await ILessonsStep.findAll({
-        order: [['createdAt', 'ASC']],
-        limit: Number(amount),
-        attributes: [
-            [Sequelize.fn('DISTINCT', Sequelize.col('questionId')) ,'questionId']
-        ]
-        
+        order: [['createdAt', 'DESC']],
+        // limit: Number(amount),
+        attributes: ['questionId', 'createdAt'],
+        where: {
+            userId
+        }
     })
 
+    const fromStepsUnique = fromSteps.map(fs => fs.get({plain: true})).reduce((acc,curr) => {
+        return {...acc, [curr.questionId]: curr.createdAt}
+    }, {})
+
+
+    const fromQuestions = await IQuestion.findAll({
+        order: [['createdAt', 'DESC']],
+        attributes: ['id', 'createdAt'],
+        where: {
+            userId
+        }
+    })
+
+    const fromQuestionsUnique = fromQuestions.map(fs => fs.get({plain: true})).reduce((acc,curr) => {
+        return {...acc, [curr.id]: curr.createdAt}
+    }, {})
+
+
+    const combined = {...fromQuestionsUnique, ...fromStepsUnique}
+
+    const combinedSliced = Object.keys(combined).map((questionId) => {
+        return {
+            questionId,
+            createdAt: combined[questionId]
+        }
+    })
+    .sort((a,b) => a.createdAt < b.createdAt ? -1 : 1)
+    .slice(0, amount)
+    .map(i => {
+        return i.questionId
+    })
     
 
+    const questions = await IQuestion.findAll({
+        where: {
+            id: {
+                [Op.in]: combinedSliced
+            }
+        }
+    })
 
-    console.log(amount)
-
-    return res.json(fromSteps)
+    return res.json({
+        combinedSliced,
+        questions
+    })
 }
